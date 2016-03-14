af1f86dc23dbf7f083a533c390e54c8fe805cad0
wghlmg1210 <wghlmg1210@sina.com>
2016-03-14 21:45:55 +0800

    encoding

--- OtherProject/pattern/designPattern/proxy/PersonHandler.java
+++ OtherProject/pattern/designPattern/proxy/PersonHandler.java
@@ -5,10 +5,8 @@ import java.lang.reflect.Method;
 
 public class PersonHandler implements InvocationHandler {
 
-	/** 被代理对象 */
 	private Object target;
 	
-	/** 被代理对象注入 */
 	public PersonHandler(Object target) {
 		this.target = target;
 	}
@@ -17,7 +15,6 @@ public class PersonHandler implements InvocationHandler {
 	public Object invoke(Object proxy, Method method, Object[] args)
 			throws Throwable {
 		System.out.println("before....");
-		/** 反射调用被代理对象函数 */
 		Object obj = method.invoke(this.target, args);
 		System.out.println("after....");
 		return obj;
